61cbac895db073098104281bd6d59a3e4f2fef44
Matt Wilkinson <mattwilki17@gmail.com>
2021-03-16 20:59:39 -0400

    errors

--- a2/adder.c
+++ a2/adder.c
@@ -120,13 +120,12 @@ void adder(int **arr, int totalNums, int numDigits)
             hasPrinted = true;
         }
     }
-    printf("\n");
+    printf("\n \n");
 
     free(carries);
     free(units);
     free(secondCarry);
     free(result);
-    exit(0);
 }
 
 // not sure how to take input
@@ -154,17 +153,29 @@ int main()
 
     printf("Enter each number in this format: 9 9 9\n");
     printf("Enter each number on a new line, hit enter a second time to add\n");
-    printf("Note: all numbers need to have the same number of digits. Enter leading zeros for shorter numbers.\n");
+    printf("Note: all numbers need to have the same number of digits. Enter leading zeros for shorter numbers.\n \n");
     while (1)
     {
         int digits;
         int totalNums;
+        int isError = 0;
         printf("Enter the number of digits in each number> ");
         scanf("%d", &digits);
+        if (digits > 10 || digits < 0)
+        {
+            printf("Error: invalid input\n");
+            continue;
+        }
+
         printf("Enter the total numbers to be added> ");
         scanf("%d", &totalNums);
+        if (totalNums > 50 || totalNums < 0)
+        {
+            printf("Error: invalid input\n");
+            continue;
+        }
 
-        printf("Enter each number to be added, with each digit seperated by a space, and each number seperated by a line.\n");
+        printf("Enter each number to be added, with each digit separated by a space, and each number separated by a line.\n");
 
         int arr[totalNums][digits];
         // char line[1024], *p, *e;
@@ -175,24 +186,22 @@ int main()
             for (int j = 0; j < digits; j++)
             {
                 scanf("%d", &arr[i][j]);
+                if (arr[i][j] < 0 || arr[i][j] > 9)
+                {
+                    printf("Error: invalid input\n");
+                    isError = 1;
+                    break;
+                }
+            }
+            if (isError == 1)
+            {
+                break;
             }
         }
-
-        // while (fgets(line, sizeof(line), stdin) != NULL)
-        // {
-        //     // if (*line == '\0' || *line == '\n')
-        //     //     break;
-        //     j = digits - 1;
-        //     for (int i = 0; i < digits; i++)
-        //     {
-        //         sscanf(line, "%d", &arr[totalNums][j]);
-        //         j--;
-        //     }
-        //     totalNums++;
-        // }
-
-        // int totalNums = sizeof(arr) / sizeof(arr[0]);
-        // int numDigits = sizeof(arr[0]) / sizeof(arr[0][0]);
+        if (isError == 1)
+        {
+            continue;
+        }
 
         int *p[totalNums];
         for (int i = 0; i < totalNums; i++)
